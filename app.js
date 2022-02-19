9f53b4c2ba05147770370c4905bf11336ad98ff1
Andriy-sys <marchycandriy@gmail.com>
2022-02-19 11:29:27 +0200

    HTML,CSS and JS

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function () {
+  var options = {
+    srcNode: 'img',
+    margin: '15px',
+    width: '240px',
+    max_width: '',
+    resizable: true,
+    transition: 'all 0.5s ease',
+  };
+  $('.grid').gridify(options);
+});
